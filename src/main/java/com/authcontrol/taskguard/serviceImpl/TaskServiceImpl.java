830af42302e6e968c2bb2b88c5d058cda5306e5a
Naga Vineel <nagavineel@Mac.lan>
2025-03-19 16:26:28 -0500

    added findById to check the user existed or not before adding the task

--- src/main/java/com/authcontrol/taskguard/serviceImpl/TaskServiceImpl.java
+++ src/main/java/com/authcontrol/taskguard/serviceImpl/TaskServiceImpl.java
@@ -1,6 +1,10 @@
 package com.authcontrol.taskguard.serviceImpl;
 
+import com.authcontrol.taskguard.entity.Task;
+import com.authcontrol.taskguard.entity.Users;
 import com.authcontrol.taskguard.payload.TaskDto;
+import com.authcontrol.taskguard.repository.TaskRepository;
+import com.authcontrol.taskguard.repository.UserRepository;
 import com.authcontrol.taskguard.service.TaskService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +18,20 @@ public class TaskServiceImpl implements TaskService {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
     @Override
     public TaskDto saveTask(long userid, TaskDto taskDto) {
-        return null;
+        Users user = userRepository.findById(userid).get();
+        Task task = modelMapper.map(taskDto, Task.class);
+        task.setUsers(user);
+        //After setting the user we are storing the data in DB
+        Task savedTask = taskRepository.save(task);
+        return modelMapper.map(savedTask, TaskDto.class);
     }
 
     @Override
